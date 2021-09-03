cffe93b13b79df1e08114f6f0c2cc0007c79cc46
Julian <aprada6@udi.edu.co>
2021-09-03 11:11:34 -0500

    TABLAS

---
+++ SQLDBPrac1/SQLDBPrac1/CURSOS.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CURSOS]
+(
+	[COD] INT NOT NULL PRIMARY KEY, 
+    [Nombre] NVARCHAR(50) NOT NULL, 
+    [Fecha_inicio] NVARCHAR(50) NOT NULL, 
+    [Duracion] INT NOT NULL, 
+    [Valor] INT NOT NULL
+)
